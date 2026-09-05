d57004b357390beafbd8e8a08d3f04cb45f06a4d
23da2-1121-png <23da2-1121@sltc.ac.lk>
2026-09-05 02:01:54 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Main execution module for Hospital Emergency Management System
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== HOSPITAL EMERGENCY MANAGEMENT SYSTEM ===");
+        System.out.println("All Modules Loaded Successfully: BST, Queue, Stack, Linked List.");
+    }
+}
